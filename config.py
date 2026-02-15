5938299cbd88b1cf400e8d71aa19a7b5a5523463
dverbg <139781644+dverbg@users.noreply.github.com>
2026-02-15 09:46:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
+# Токен бота Telegram
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Параметры MySQL (берутся из переменных среды Railway)
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = int(os.getenv("DB_PORT", 3306))
-CRYPTO_TOKEN = os.getenv("CRYPTO_TOKEN")
