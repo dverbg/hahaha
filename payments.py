edd37ebf2ecf6387df998f10a8db3248d2504123
dverbg <139781644+dverbg@users.noreply.github.com>
2026-02-15 08:14:39 +0100

    Update payments.py

--- payments.py
+++ payments.py
@@ -1,16 +1,14 @@
-import aiohttp,config
+import aiohttp
+import config
 
-async def create_invoice(amount,user_id):
-    async with aiohttp.ClientSession() as s:
-        headers={"Crypto-Pay-API-Token":config.CRYPTO_TOKEN}
-
-        data={
-            "asset":"USDT",
-            "amount":amount,
-            "payload":str(user_id)
+async def create_invoice(amount, user_id):
+    async with aiohttp.ClientSession() as session:
+        headers = {"Crypto-Pay-API-Token": config.CRYPTO_TOKEN}
+        data = {
+            "asset": "USDT",
+            "amount": amount,
+            "payload": str(user_id)
         }
-
-        async with s.post("https://pay.crypt.bot/api/createInvoice",
-                          json=data,headers=headers) as r:
-            res=await r.json()
-            return res["result"]["pay_url"],res["result"]["invoice_id"]
+        async with session.post("https://pay.crypt.bot/api/createInvoice", json=data, headers=headers) as r:
+            res = await r.json()
+            return res["result"]["pay_url"], res["result"]["invoice_id"]
