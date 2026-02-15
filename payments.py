db7caf6715fd51ac8ec53898786c29097c4b4026
dverbg <139781644+dverbg@users.noreply.github.com>
2026-02-15 07:47:16 +0100

    Create payments.py

---
+++ payments.py
@@ -0,0 +1,16 @@
+import aiohttp,config
+
+async def create_invoice(amount,user_id):
+    async with aiohttp.ClientSession() as s:
+        headers={"Crypto-Pay-API-Token":config.CRYPTO_TOKEN}
+
+        data={
+            "asset":"USDT",
+            "amount":amount,
+            "payload":str(user_id)
+        }
+
+        async with s.post("https://pay.crypt.bot/api/createInvoice",
+                          json=data,headers=headers) as r:
+            res=await r.json()
+            return res["result"]["pay_url"],res["result"]["invoice_id"]
